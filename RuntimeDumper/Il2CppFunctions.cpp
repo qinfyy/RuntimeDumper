074f7a3c976cbda121f5e59b7c5c1e1437acec8e
Cyt <qinfyy233@gmail.com>
2026-01-16 19:11:57 +0800

    adding failure message

--- RuntimeDumper/Il2CppFunctions.cpp
+++ RuntimeDumper/Il2CppFunctions.cpp
@@ -2,23 +2,40 @@
 #include "Il2CppFunctions.h"
 #include <iostream>
 #include "PrintHelper.h"
+#include <sstream>
 
 void InitIl2CppFunctions()
 {
     HMODULE hGameAssembly = GetModuleHandleA("GameAssembly.dll");
     if (!hGameAssembly) {
-		MessageBoxA(NULL, "GameAssembly.dll not found!", "Error", MB_OK | MB_ICONERROR);
-		return;
+        MessageBoxA(NULL, "GameAssembly.dll not found!", "Error", MB_OK | MB_ICONERROR);
+        ExitProcess(1);
+        return;
     }
 
+    int totalApi = 0;
+    int failedApi = 0;
+
 #define DO_API(r, n, p) \
-        n = (r (*) p) GetProcAddress(hGameAssembly, #n); \
-        DebugPrintA("[IL2CPP] %-55s -> %p\n", #n, n);
+    totalApi++; \
+    n = (r (*) p) GetProcAddress(hGameAssembly, #n); \
+    DebugPrintA("[IL2CPP] %-55s -> %p\n", #n, n); \
+    if (!n) failedApi++;
 
 #define DO_API_NO_RETURN(r, n, p) DO_API(r, n, p)
 #include "il2cpp-api-functions.h"
 #undef DO_API
 #undef DO_API_NO_RETURN
+
+    if (failedApi > 0) {
+        std::stringstream ss;
+        ss << "Detected " << failedApi << "/" << totalApi
+            << " IL2CPP APIs failed to load!" << std::endl
+            << "Please check your game Unity version or whether the game is encrypted/protected.";
+
+        MessageBoxA(NULL, ss.str().c_str(), "IL2CPP API Binding Error", MB_OK | MB_ICONERROR);
+        ExitProcess(1);
+    }
 }
 
 bool AttachIl2Cpp(Il2CppDomain*& outDomain, Il2CppThread*& outThread)
